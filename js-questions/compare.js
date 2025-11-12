224851cf6cf5dea52ab18193f0b6e2b3740831a8
gourab8389 <deyg6988@gmail.com>
2025-11-12 23:36:45 +0530

    Add additional comparison examples in compare.js

--- js-questions/compare.js
+++ js-questions/compare.js
@@ -2,3 +2,9 @@ console.log(5 == '5');   // true (type coercion)
 console.log(5 === '5');  // false (different types)
 console.log(null == undefined);  // true
 console.log(null === undefined); // false
+console.log(NaN == NaN); // false
+console.log(Object.is(NaN, NaN)); // true
+console.log(Object.is(0, -0)); // false
+console.log(0 === -0); // true
+
+console.log([] == false); // true
